d52597d4457254f09a2ea9948e078b98655cb6bf
bunty <buntyraj568@gmail.com>
2021-06-24 15:43:14 -0400

    Initial commit

---
+++ src/test/java/com/example/MemberRecord/MemberRecordApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.MemberRecord;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MemberRecordApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
